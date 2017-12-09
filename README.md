dec5639729ae658077cba5df81a0d1890cdfe05b
Chris Huson <chrishuson@rocketmail.com>
2017-12-08 16:34:04 -0800

    add more else if statements, and readme.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Beep Boop
+
+##### By Chris Huson
+
+### Specification
+- input 2 return 1,2
+- returns 1's with beep
+- returns numbers with 0 as boop
+- returns numbers divisible by 3 with "I'm sorry, Dave. I'm afraid I can't do that."
+
+### Created with atom.
+
+### Languages used: HTML, Javascript, Bootstrap, Jquery.
+
+### Known bugs: Will not equate 100.
+
+### Contact Chris with any questions.
+
+##### Copywrite MIT 2016.
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 function beepBoop(userInput){
 var result = [];
 for (var i = 1; i <= userInput; i++){
-  result.push (i);
+  if (i === 1){
+    result.push ("beep");
+  } else if ((i % 10) === 0){
+    result.push ("boop");
+  } else if ((i % 3) === 0){
+    result.push ("I'm sorry, Dave. I'm afraid I can't do that.");
+  } else
+    result.push (i);
 }
 return result;
 }
+
 
 $(document).ready(function() {
   $("form#beep").submit(function(event) {
     $("#res").empty();
     var userInput = parseInt($("input#userNumber").val());
     var result = beepBoop(userInput);
-
     result.forEach(function(output) {
     $("#userOutput").append("<li>" + output + "</li>");
     event.preventDefault();
